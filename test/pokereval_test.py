f446c5939566778864fa87b79c67855b3943d2be
Alvin Liang <alvin@ayl-mb.local>
2011-01-17 18:12:33 -0500

    some changes to tests and benchmarks

--- test/pokereval_test.py
+++ test/pokereval_test.py
@@ -1,9 +1,10 @@
 import sys
 sys.path.append("../")
 import random
+import math
 from itertools import combinations
 import pokereval
-from pokerbots.player.hand_evaluator import HandEvaluator
+from pokerbots.player.hand_evaluator_python import HandEvaluator
 from pokerbots.engine.game import Card
 
 deck = set((
@@ -23,7 +24,7 @@ deck = set((
 ))
 
 pe = pokereval.PokerEval()
-number_of_runs = 100
+number_of_runs = 10
 to_str = lambda c: c.__str__()
 
 def pokereval_5_bm():
@@ -59,42 +60,55 @@ def handevaluator_5_bm():
         board = cards[2:5]
         HandEvaluator.evaluate_hand(hand, board)
 
-def _pokereval_evaluate_hand_bm(cards):
-    hand = map(to_str, cards[0:2])
-    board = map(to_str, cards[2:7])
-    return pe.poker_eval(game="holdem", pockets=[hand, [255,255]],
-        dead=[], board=board)
+def _pokereval_evaluate_hand_bm(hand, board, iterations=None):
+    hand = map(to_str, hand)
+    board = map(to_str, board)
+    for i in xrange(5 - len(board)):
+        board.append("__")
+    if iterations:
+        return pe.poker_eval(game="holdem", pockets=[hand, [255,255]],
+            dead=[], board=board, iterations=iterations)
+    else:
+        return pe.poker_eval(game="holdem", pockets=[hand, [255,255]],
+            dead=[], board=board)
 
-def _winners_evaluate_hand_bm(cards):
-    hand = map(to_str, cards[0:2])
-    board = map(to_str, cards[2:7])
-    possible_opponent_hands = list(combinations(deck - set(cards), 2))
-    hands_beaten = 0
-    for opponent_hand in possible_opponent_hands:
-        ohand = map(to_str, opponent_hand)
-        per = pe.winners(game="holdem", pockets=[hand, ohand],
-            dead=hand + ohand, board=board)
-        if len(per['hi']) == 2: # draw
-            hands_beaten += 0.5
-        elif per['hi'][0] == 0:
-            # player won this iteration
-            hands_beaten += 1
-    return float(hands_beaten) / len(possible_opponent_hands)
+def pokereval_convergence(num_cards=7):
+    for i in xrange(number_of_runs):
+        cards = random.sample(deck, num_cards)
+        hand = cards[0:2]
+        board = cards[2:num_cards]
+        for i in (100,300,500,1000,5000,None):
+            if i:
+                pe_result = _pokereval_evaluate_hand_bm(hand, board, iterations = i)
+            else:
+                pe_result = _pokereval_evaluate_hand_bm(hand, board)
+            pe_win_pct = pe_result['eval'][0]['winhi'] / float(pe_result['info'][0])
+            pe_ev = pe_result['eval'][0]['ev']
+        
+            # print pe_result
+        
+            print "PokerEval win pct is %s, EV is %s (%s iterations)" % (pe_win_pct,
+                pe_ev, pe_result['info'][0])
 
-def pokereval_results(print_to_screen=False):
+def pokereval_results(num_cards=7, print_to_screen=False):
     pe = pokereval.PokerEval()
     to_str = lambda c: c.__str__()
     for i in xrange(number_of_runs):
-        cards = random.sample(deck, 7)
+        cards = random.sample(deck, num_cards)
         hand = cards[0:2]
-        board = cards[2:7]
+        # hand = [Card(14,1), Card(14,2)]
+        board = cards[2:num_cards]
         
         he_result = HandEvaluator.evaluate_hand(hand, board)
-        pe_result = _pokereval_evaluate_hand_bm(cards)
-        pe_winners = _winners_evaluate_hand_bm(cards)
+        pe_result = _pokereval_evaluate_hand_bm(hand, board, 1000)
         
-        if print_to_screen:
-            print "PokerEval.poker_eval got %s, .winners got %s, HandEvaluator got %s" % \
-                (pe_result['eval'][0]['ev']/1000.0, pe_winners, he_result)
+        pe_win_pct = pe_result['eval'][0]['winhi'] / float(pe_result['info'][0])
+        pe_ev = pe_result['eval'][0]['ev']
+        
+        # print pe_result
+        
+        #print "PokerEval win pct is %s, EV is %s (%s iterations)" % (pe_win_pct,
+            #pe_ev, pe_result['info'][0])
 
-pokereval_results()
+# pokereval_results(5, print_to_screen=True)
+pokereval_convergence(7)
