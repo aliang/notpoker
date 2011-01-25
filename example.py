c49cc0b2dbd913e80feddd27cab1190a262e7f52
Alvin Liang <alvin@ayl-mb.local>
2011-01-24 22:34:00 -0500

    stub out logger

--- example.py
+++ example.py
@@ -2,16 +2,37 @@ from pokerbots.engine.game import Table
 from pokerbots.player.pokerbot import Pokerbot
 import time
 
+STUB_OUT_LOGGER = False
+
 p1_name = 'masterchefC'
-p2_name = 'bleedbot'
+p2_name = 'masterchefC'
 p1 = Pokerbot(p1_name)
 p2 = Pokerbot(p2_name)
 p1_wins = 0
 p2_wins = 0
 num_matches = 100
 
+def stub(*args, **kwargs):
+    pass
+
 for i in range(num_matches):
     t = Table(p1, p2)
+    if STUB_OUT_LOGGER:
+        t.logger.action = stub
+        t.logger.begin_hand = stub
+        t.logger.blinds = stub
+        t.logger.end = stub
+        t.logger.file_one = stub
+        t.logger.file_two = stub
+        t.logger.preflop = stub
+        t.logger.refund = stub
+        t.logger.results = stub
+        t.logger.showdown = stub
+        t.logger.street = stub
+        t.logger.time = stub
+        t.logger.write_both = stub
+        t.logger.write_one = stub
+        t.logger.write_two = stub
     t.play_match()
     state = t.state()
     if state['players'][0]['stack'] == 800:
